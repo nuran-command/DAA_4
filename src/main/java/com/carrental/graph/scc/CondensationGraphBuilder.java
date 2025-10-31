2f9f5a9c0a13ef7093bf7743fd32ae5f2dfb5b63
Nurdan Zhanabaev <nurdanzanabaev@gmail.com>
2025-10-31 14:36:01 +0500

    add: CondensationGraphBuilder for DAG construction

--- src/main/java/com/carrental/graph/scc/CondensationGraphBuilder.java
+++ src/main/java/com/carrental/graph/scc/CondensationGraphBuilder.java
@@ -1,4 +1,32 @@
 package com.carrental.graph.scc;
 
+import com.carrental.graph.util.Graph;
+import java.util.*;
+
 public class CondensationGraphBuilder {
+
+    public Graph buildCondensation(Graph g, List<Component> sccs) {
+        Map<Integer, Integer> nodeToComp = new HashMap<>();
+        for (Component c : sccs) {
+            for (int node : c.getNodes()) {
+                nodeToComp.put(node, c.getId());
+            }
+        }
+
+        Graph dag = new Graph();
+        for (Component c : sccs) {
+            dag.addVertex(c.getId());
+        }
+
+        for (int u : g.getVertices()) {
+            for (int v : g.getAdj(u)) {
+                int cu = nodeToComp.get(u);
+                int cv = nodeToComp.get(v);
+                if (cu != cv) {
+                    dag.addEdge(cu, cv);
+                }
+            }
+        }
+        return dag;
+    }
 }
