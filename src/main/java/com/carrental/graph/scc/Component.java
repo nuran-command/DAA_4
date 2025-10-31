dd6ef31c6ed15b587c43af08f93a862accbb0d82
Nurdan Zhanabaev <nurdanzanabaev@gmail.com>
2025-10-31 14:31:58 +0500

    add: Component class for SCC storage

--- src/main/java/com/carrental/graph/scc/Component.java
+++ src/main/java/com/carrental/graph/scc/Component.java
@@ -1,4 +1,26 @@
 package com.carrental.graph.scc;
 
+import java.util.List;
+
 public class Component {
+    private final int id;
+    private final List<Integer> nodes;
+
+    public Component(int id, List<Integer> nodes) {
+        this.id = id;
+        this.nodes = nodes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Integer> getNodes() {
+        return nodes;
+    }
+
+    @Override
+    public String toString() {
+        return "Component " + id + ": " + nodes;
+    }
 }
