9525b540736092a6369e212f9ddc22dd54d3b5a7
Nurdan Zhanabaev <nurdanzanabaev@gmail.com>
2025-11-02 15:31:07 +0500

    add: added some tests for topo

--- src/test/java/com/carrental/graph/topo/TopologicalSorterTest.java
+++ src/test/java/com/carrental/graph/topo/TopologicalSorterTest.java
@@ -45,4 +45,59 @@ class TopologicalSorterTest {
         List<Integer> order = sorter.sort(g);
         assertNotNull(order);
     }
+    @Test
+    void testDisconnectedGraph() {
+        Graph g = new Graph();
+        g.addEdge(0, 1);
+        g.addVertex(2); // isolated vertex
+        g.addVertex(3); // another disconnected vertex
+
+        TopologicalSorter sorter = new TopologicalSorter();
+        List<Integer> order = sorter.sort(g);
+
+        assertEquals(4, order.size());
+        assertTrue(order.containsAll(List.of(0, 1, 2, 3)));
+    }
+
+    @Test
+    void testSingleVertexGraph() {
+        Graph g = new Graph();
+        g.addVertex(5);
+
+        TopologicalSorter sorter = new TopologicalSorter();
+        List<Integer> order = sorter.sort(g);
+
+        assertEquals(List.of(5), order);
+    }
+
+    @Test
+    void testLargeLinearGraph() {
+        Graph g = new Graph();
+        int n = 500;
+        for (int i = 0; i < n - 1; i++) g.addEdge(i, i + 1);
+        TopologicalSorter sorter = new TopologicalSorter();
+        List<Integer> order = sorter.sort(g);
+
+        assertEquals(n, order.size());
+        // because reversed: last vertex comes first
+        assertEquals(n - 1, order.get(0));
+        assertEquals(0, order.get(n - 1));
+    }
+
+    @Test
+    void testGraphWithBranching() {
+        Graph g = new Graph();
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+
+        TopologicalSorter sorter = new TopologicalSorter();
+        List<Integer> order = sorter.sort(g);
+
+        // In reversed output, dependencies appear later
+        assertTrue(order.indexOf(3) < order.indexOf(1));
+        assertTrue(order.indexOf(3) < order.indexOf(2));
+        assertTrue(order.indexOf(1) < order.indexOf(0) || order.indexOf(2) < order.indexOf(0));
+    }
 }
